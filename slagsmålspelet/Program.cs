54f31e5b63f020c11aefad2b920c2e32c5546558
Anton Faren <Anton.faren@elev.ga.ntig.se>
2024-09-18 14:13:05 +0200

    first commit

---
+++ slagsmålspelet/Program.cs
@@ -0,0 +1,31 @@
+﻿int hp1 = 100;
+int hp2 = 100;
+
+Console.Write("vad är ditt namn? ");
+String name = Console.ReadLine();
+Console.WriteLine($"Välkomen {name} till slagsmåls spelet");
+Thread.Sleep(2000);
+Console.WriteLine($"{name} vi behöver din styrka att besegra den onde greven");
+Thread.Sleep(1000);
+Console.WriteLine("Vill du hjälpa?");
+Thread.Sleep(1000);
+Console.WriteLine("Yes/No");
+
+string acceptering = Console.ReadLine().ToLower();
+
+if (acceptering == "yes"){
+    Console.WriteLine("Bra att du vill");
+}
+else {
+Console.WriteLine("Nerver back down never give up , du får ändå");
+}
+
+while (true)
+{
+    
+
+    
+}
+
+
+Console.ReadLine();
