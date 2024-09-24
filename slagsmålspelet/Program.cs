2d58c291a9dd354afa7b22942ced8b7385b1a9d6
Anton Faren <Anton.faren@elev.ga.ntig.se>
2024-09-24 14:27:45 +0200

    slagsmålspel

--- slagsmålspelet/Program.cs
+++ slagsmålspelet/Program.cs
@@ -1,31 +1,87 @@
-﻿int hp1 = 100;
-int hp2 = 100;
-
-Console.Write("vad är ditt namn? ");
-String name = Console.ReadLine();
-Console.WriteLine($"Välkomen {name} till slagsmåls spelet");
-Thread.Sleep(2000);
-Console.WriteLine($"{name} vi behöver din styrka att besegra den onde greven");
-Thread.Sleep(1000);
-Console.WriteLine("Vill du hjälpa?");
-Thread.Sleep(1000);
-Console.WriteLine("Yes/No");
-
-string acceptering = Console.ReadLine().ToLower();
-
-if (acceptering == "yes"){
-    Console.WriteLine("Bra att du vill");
-}
-else {
-Console.WriteLine("Nerver back down never give up , du får ändå");
-}
-
+﻿
 while (true)
 {
-    
+   int hp2 = 100;
+   int hp1 = 100;
 
-    
+   Console.Write("vad är ditt namn? ");
+   String name = Console.ReadLine();
+
+   while (name == "")
+   {
+      Console.WriteLine("skriv något namn snälla");
+      Console.WriteLine("försök igen:");
+      name = Console.ReadLine();
+   }
+   Console.Clear();
+   Console.WriteLine($"Välkomen {name} till slagsmåls spelet");
+   Thread.Sleep(2000);
+   Console.Clear();
+   Console.WriteLine($"{name} vi behöver din styrka att besegra den onde greven");
+   Thread.Sleep(1000);
+   Console.WriteLine("Vill du hjälpa?");
+   Thread.Sleep(1000);
+   Console.WriteLine("Yes/No");
+
+
+   string acceptering = Console.ReadLine().ToLower();
+
+   if (acceptering == "yes")
+   {
+      Console.WriteLine("Bra att du vill");
+   }
+   else
+   {
+      Console.WriteLine("Nerver back down never give up , du får ändå");
+   }
+
+   Thread.Sleep(1000);
+   Console.Clear();
+
+   while (true)
+   {
+
+      int at1 = Random.Shared.Next(0, 26);
+      int at2 = Random.Shared.Next(0, 21);
+      
+      if (hp1 > 0 && hp2 > 0)
+      {
+         hp1 -= at2;
+         hp2 -= at1;
+         Console.WriteLine($"{name} gjorde {at1}dmg, Greven har {hp2} kvar");
+         Console.WriteLine($"Greven gjorde {at2}dmg, {name} har {hp1} kvar");
+
+         Console.ReadLine();
+         Console.Clear();
+      }
+
+
+      if (at1 == 7)
+      {
+         Console.WriteLine("Ronadlo kom med i fighten och besgrade greven med en enda spark");
+         Console.WriteLine($"{name} och Ronaldo van");
+         Console.ReadLine();
+         break;
+      }
+      if (hp1 <= 0)
+      {
+         Console.WriteLine("Greven besegrade dig");
+         Console.ReadLine();
+         break;
+      }
+      if (hp2 <= 0)
+      {
+         Console.WriteLine($"{name} besegrade greven , bra för dig");
+         Console.ReadLine();
+         break;
+
+      }
+
+
+
+   }
+
+   Console.WriteLine("Tryck på enter knappen för att börja om");
+   Console.ReadLine();
+   Console.Clear();
 }
-
-
-Console.ReadLine();
--- slagsmålspelet/obj/Debug/net8.0/slagsmålspelet.AssemblyInfo.cs
+++ slagsmålspelet/obj/Debug/net8.0/slagsmålspelet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("slagsmålspelet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54f31e5b63f020c11aefad2b920c2e32c5546558")]
 [assembly: System.Reflection.AssemblyProductAttribute("slagsmålspelet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("slagsmålspelet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
