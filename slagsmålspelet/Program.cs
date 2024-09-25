24bb3f65a2b92f5677cdbada7e81a0996efca7e4
Anton Faren <Anton.faren@elev.ga.ntig.se>
2024-09-25 15:00:43 +0200

    uppdateringar på slagsmålspel

--- slagsmålspelet/Program.cs
+++ slagsmålspelet/Program.cs
@@ -3,6 +3,9 @@ while (true)
 {
    int hp2 = 100;
    int hp1 = 100;
+   int hp3 = 50;
+   int runda = 1;
+
 
    Console.Write("vad är ditt namn? ");
    String name = Console.ReadLine();
@@ -41,20 +44,22 @@ while (true)
    while (true)
    {
 
-      int at1 = Random.Shared.Next(0, 26);
+      int at1 = Random.Shared.Next(5, 26);
       int at2 = Random.Shared.Next(0, 21);
       
       if (hp1 > 0 && hp2 > 0)
       {
          hp1 -= at2;
          hp2 -= at1;
+         Console.WriteLine($"Runda {runda}");
          Console.WriteLine($"{name} gjorde {at1}dmg, Greven har {hp2} kvar");
          Console.WriteLine($"Greven gjorde {at2}dmg, {name} har {hp1} kvar");
 
          Console.ReadLine();
          Console.Clear();
+         runda += 1;
       }
-
+   
 
       if (at1 == 7)
       {
@@ -63,6 +68,10 @@ while (true)
          Console.ReadLine();
          break;
       }
+       if (hp1 <= 0 && hp2 <= 0){
+         Console.WriteLine("det blev ovagjort ni tog ut varandra samtidigt");
+         break;
+   }
       if (hp1 <= 0)
       {
          Console.WriteLine("Greven besegrade dig");
@@ -76,10 +85,47 @@ while (true)
          break;
 
       }
+     
 
 
 
    }
+   if(hp1 > 0){
+Thread.Sleep(1000);
+Console.WriteLine("Åh nej , en gamal tant börjar slå dig med sin handväska ");
+Console.WriteLine("Nästa boss är en gammal tant!");
+Console.ReadLine();
+Console.Clear();
+   }
+
+   while (true)
+   {
+     int at1 = Random.Shared.Next(5, 26);
+      int at3 = Random.Shared.Next(0, 11);
+      
+      if (hp1 > 0 && hp3 > 0)
+      {
+         hp1 -= at3;
+         hp3 -= at1;
+         Console.WriteLine($"Runda {runda}");
+         Console.WriteLine($"{name} gjorde {at1}dmg, Gamla tanten har {hp3} kvar");
+         Console.WriteLine($"Gammla tanten gjorde {at3}dmg, {name} har {hp1} kvar");
+
+         Console.ReadLine();
+         Console.Clear();
+         runda += 1;
+      }  
+      if (hp1 < 0){
+         Console.WriteLine("Du blev dödad av tanten");
+      break;
+      }
+      if (hp3 < 0){
+         Console.WriteLine("Du besegrade tanten");
+         break;
+      }
+   }
+
+
 
    Console.WriteLine("Tryck på enter knappen för att börja om");
    Console.ReadLine();
